909c82936cb0ddcd99bcfedca9c64e82bcfb08e8
supalapka <yurabalint0103@gmail.com>
2023-09-21 01:11:01 +0300

    Added list of coins

---
+++ src/utils.js
@@ -0,0 +1,34 @@
+export const allCryptoSymbols = [
+    "BTC",
+    "ETH",
+    "BNB",
+    "ATOM",
+    "SOL",
+    "XRP",
+  
+    "MATIC",
+    "ADA",
+    "DOGE",
+    "TRX",
+    "AVAX",
+    "LTC",
+  
+    "LINK",
+    "APT",
+    "MANA",
+    "AAVE",
+    "DASH",
+    "CAKE",
+  
+    "UNI",
+    "XMR",
+    "FTM",
+    "EOS",
+    "STX",
+    "QNT",
+  
+    "NEO",
+    "SUSHI",
+    "XLM",
+    "FIL",
+  ];

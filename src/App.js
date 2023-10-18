8b9522defe87655d9fc2c8c7281b81715825ff74
supalapka <yurabalint0103@gmail.com>
2023-10-19 02:29:37 +0300

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./css/main.css"
 import Header from "./components/Header";
+import { router } from "./router";
+
 
 class App extends React.Component {
   render() {
+
     return (
       <div>
         <Header />
-        <h1>Hello crypto exhange</h1>
+        <div className="content">
+          {router()}
+        </div>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App"
+import { BrowserRouter } from "react-router-dom";
 
 const app = ReactDOMClient.createRoot(document.getElementById("root"))
 
-app.render(<App/>)
+app.render(
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+)
